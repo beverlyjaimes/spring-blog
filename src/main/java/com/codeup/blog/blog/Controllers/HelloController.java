f43e226185c77b754fe049ec870f397854c3a0c0
beverlyjaimes <beverly.jaimespuente@gmail.com>
2019-11-05 11:45:24 -0600

    after lecture

--- src/main/java/com/codeup/blog/blog/Controllers/HelloController.java
+++ src/main/java/com/codeup/blog/blog/Controllers/HelloController.java
@@ -1,7 +1,10 @@
 package com.codeup.blog.blog.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @Controller
 public class HelloController {
@@ -11,18 +14,52 @@ public class HelloController {
     public String sayHello(){
         return "hey";
     }
+//
+//    @GetMapping("/hello/{name}")
+//    @ResponseBody
+//    public String sayHelloWithName(@PathVariable String name){
+//        System.out.println("name = " + name);
+//        return "<h2>hey " + name + "</h2>";
+//    }
+
 
     @GetMapping("/hello/{name}")
-    @ResponseBody
-    public String sayHelloWithName(@PathVariable String name){
-        System.out.println("name = " + name);
-        return "<h2>hey " + name + "</h2>";
+    public String sayHelloWithName(@PathVariable String name, Model viewModel){
+        viewModel.addAttribute("name", name);
+        return "hello";
+//hello is the name of the file
     }
+
 
     @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
     @ResponseBody
     public String addOne(@PathVariable int number) {
         return number + " plus one is " + (number + 1) + "!";
+    }
+
+    @GetMapping("/join")
+    public String showJoinForm() {
+        return "join";
+    }
+
+    @PostMapping("/join")
+    public String joinCohort(@RequestParam(name = "cohort") String cohort, Model vModel) {
+        ArrayList<String> students = new ArrayList<>();
+
+        students.add("Mark");
+        students.add("Beth");
+        students.add("Eddy");
+
+
+        vModel.addAttribute("cohort", "Welcome to " + cohort + "!");
+        vModel.addAttribute("students", students);
+        return "join";
+
+//        model means nothing you can name it whatever you want but its not the MVC model.
+
+
+
+
     }
 }
 
--- src/main/java/com/codeup/blog/blog/Controllers/HomeController.java
+++ src/main/java/com/codeup/blog/blog/Controllers/HomeController.java
@@ -1,15 +1,31 @@
 package com.codeup.blog.blog.Controllers;
 
 
+import com.codeup.blog.blog.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.swing.text.AbstractDocument;
+import java.util.ArrayList;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    @ResponseBody
-    public String landing(){return "This is the landing page";}
+        public String index (Model viewModel){
+            ArrayList<Post> postList = new ArrayList<Post>();
+            postList.add(new Post(1, "fist post", "new"));
+            postList.add(new Post(2, "second post", "Second post"));
+
+
+            viewModel.addAttribute("posts", postList);
+
+
+            return "home";
+        }
+
 
 }
+
