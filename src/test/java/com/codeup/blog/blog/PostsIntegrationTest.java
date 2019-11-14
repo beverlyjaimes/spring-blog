e3188f59085ea3e3e5b5e41f2a2dff9ad3f9797d
beverlyjaimes <beverly.jaimespuente@gmail.com>
2019-11-14 11:50:17 -0600

    test one

---
+++ src/test/java/com/codeup/blog/blog/PostsIntegrationTest.java
@@ -0,0 +1,81 @@
+package com.codeup.blog.blog;
+
+import com.codeup.blog.blog.BlogApplication;
+import com.codeup.blog.blog.Repositories.PostRepository;
+import com.codeup.blog.blog.Repositories.UserRepository;
+import com.codeup.blog.blog.User;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.servlet.http.HttpSession;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = BlogApplication.class)
+@AutoConfigureMockMvc
+public class PostsIntegrationTest {
+
+    private User testUser;
+    private HttpSession httpSession;
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    UserRepository userDao;
+
+    @Autowired
+    PostRepository postsDao;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Before
+    public void setup() throws Exception {
+
+        testUser = userDao.findByUsername("testUser");
+
+        // Creates the test user if not exists
+        if(testUser == null){
+            User newUser = new User();
+            newUser.setUsername("testUser");
+            newUser.setPassword(passwordEncoder.encode("pass"));
+            newUser.setEmail("testUser@codeup.com");
+            testUser = userDao.save(newUser);
+        }
+
+        // Throws a Post request to /login and expect a redirection to the Ads index page after being logged in
+        httpSession = this.mvc.perform(post("/login").with(csrf())
+                .param("username", "testUser")
+                .param("password", "pass"))
+                .andExpect(status().is(HttpStatus.FOUND.value()))
+                .andExpect(redirectedUrl("/posts/"))
+                .andReturn()
+                .getRequest()
+                .getSession();
+    }
+
+//    the test will fail the point it to make sure it creates a post
+
+    @Test
+    public void testIfPostCanBeCreated() throws Exception{
+        this.mvc.perform(post("/posts/create").with(csrf())
+                .session((MockHttpSession) this.httpSession)
+                .param("title", "a new post")
+                .param("body", "asdasdasd"))
+                .andExpect(status().is3xxRedirection());
+    }
+}
